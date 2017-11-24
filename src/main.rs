3d9ce23eca52fb63893a4d041a95a5ba8187714c
Rubin Simons <rubin@raaftech.com>
2017-11-24 18:53:56 +0100

    Unicode hello.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn greet() {
+
+    let a = "Hello, world!";
+    let b = "Grüß Gott!";
+    let c = "ハロー・ワールド";
+
+    let regions = [a, b ,c];
+
+    for region in regions.iter() {
+            println!("{}", &region);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet();
 }
