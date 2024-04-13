389775fad98f3361c0f5fa66a66fa1d25614ebb1
Rubin Simons <me@rubin55.org>
2024-04-13 19:28:06 +0200

    Example usage of generic function.

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,9 +1,12 @@
 use std::ops::Add;
-
+use std::time::Duration;
 fn add<T: Add<Output = T>>(a: T, b: T) -> T {
     a.add(b) // is the same as a + b
 }
 
 fn main() {
-    println!("Yes: {}", add(52, 71));
+    println!("Durations: {:?}", add(Duration::new(5, 0), Duration::new(10, 0)));
+    println!("Integers: {}", add(52, 71));
+    println!("Floats: {}", add(3.3, 4.6));
 }
+cursor
