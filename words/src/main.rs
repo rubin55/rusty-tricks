e9a33f59dea4d04b34f812701c63bd6234f6a38f
Rubin Simons <me@rubin55.org>
2024-05-08 15:52:38 +0200

    Using entry.

--- words/src/main.rs
+++ words/src/main.rs
@@ -5,11 +5,8 @@ fn main() {
     let mut freqs = HashMap::new();
 
     for word in text.split_whitespace() {
-        match freqs.get_mut(word) {
-            Some(value) => *value +=1,
-            None => { freqs.insert(word, 1)}
-        }
+        *freqs.entry(word).or_insert(0) += 1;
     }
 
-    println!("Hello, world!");
+    println!("words: {:?}", freqs);
 }
