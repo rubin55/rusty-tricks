40902db59c194ad795e3857ff9902e590849cd74
Rubin Simons <me@rubin55.org>
2024-05-13 12:42:50 +0200

    Do something with Err and Ok.

--- result/src/main.rs
+++ result/src/main.rs
@@ -9,9 +9,23 @@ struct Person {
 fn main() {
   // Using json! macro.
   let first = Person::deserialize(json!({ "name": "Margaret Hamilton" }));
-  println!("First: {:?}", first);
+
+  // Don't handle error, everything but Ok -> unimplemented. Note: could also first.unwrap_or_default().
+  let first_inner = match first {
+    Ok(x) => x,
+    _ => unimplemented!()
+  };
+
+  println!("First: {:?}", first_inner.name);
 
   // Using from_str.
   let second = serde_json::from_str::<Person>(r#"{ "name": "Johnny Example", }"#);
-  println!("Sedcond: {:?}", second);
+
+  // Handle error, print "Unknown" instead.  Note: could also second.unwrap_or_default().
+  let second_inner = match second {
+    Ok(x) => x,
+    Err(_) => Person { name: String::from("Unknown")}
+  };
+
+  println!("Second: {:?}", second_inner.name);
 }
